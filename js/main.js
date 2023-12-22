e6762d8faa2879693d124c6e29970eb1e8003fdf
Gael Ramos <esgaelramos@gmail.com>
2023-12-21 23:10:14 -0600

    add main js with function for typewriter

---
+++ js/main.js
@@ -0,0 +1,45 @@
+// ---> TYPEWRITER <--- //
+document.addEventListener('DOMContentLoaded',function(event) {
+  // array with texts to type in typewriter
+  var dataText = ["want to develop Backend", "like to design DataBases", "enjoy Learning", "don't speak English", "say Hello!"];
+
+  // type one text in the typwriter
+  // keeps calling itself until the text is finished
+  function typeWriter(text, i, fnCallback){
+    // chekc if text isn't finished yet
+    if (i < (text.length)) {
+      // add next character to <>
+      document.querySelector("typewriter").innerHTML = text.substring(0, i+1) +'<span class="cursor-text" aria-hidden="true"></span>';
+
+      // wait for a while and call this function again for next character
+      setTimeout(function() {
+        typeWriter(text, i + 1, fnCallback)
+      }, 100);
+    }
+    // text finished, call callback if there is a callback function
+    else if (typeof fnCallback == 'function') {
+      // call callback after timeout
+      setTimeout(fnCallback, 700);
+    }
+  }
+  // start a typewriter animation for a text in the dataText array
+    function StartTextAnimation(i) {
+      if (typeof dataText[i] == 'undefined') {
+        setTimeout(function() {
+          StartTextAnimation(0);
+        }, 7000);
+      }
+      // check if dataText[i] exists
+    if (i < dataText[i].length) {
+      // text exists! start typewriter animation
+      typeWriter(dataText[i], 0, function() {
+        // after callback (and whole text has been animated), start next text
+        StartTextAnimation(i + 1);
+      });
+    }
+  }
+  // start the text animation
+  setTimeout(function() {
+    StartTextAnimation(0);
+  }, 2000);
+});
